cdc6b05c928dac5ea056c74e344a7cfb22a88389
Dave Rapin <dave@rapin.com>
2015-10-16 10:37:22 -0400

    Added android path

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,4 +1,5 @@
 export GOPATH=$HOME/go
+export ANDROID_HOME=/usr/local/opt/android-sdk
 
 # path, the 0 in the filename causes this to load first
 export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr:./node_modules:$HOME/bin:$GOPATH/bin
