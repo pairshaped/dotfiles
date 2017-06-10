0932de90b723e145ac91e2e4eac851490d068d08
Dave Rapin <dave@rapin.com>
2017-06-10 13:17:30 +0000

    Fixed vim colors when in tmux

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -1,4 +1,6 @@
 # Load any custom after code
+export TERM="xterm-256color"
+
 if [ -d $HOME/.zsh.after/ ]; then
   if [ "$(ls -A $HOME/.zsh.after/)" ]; then
     for config_file ($HOME/.zsh.after/*.zsh) source $config_file
