78bd865a2781be1c8018c6618ee7ac4b11e732eb
Dave Rapin <dave@rapin.com>
2017-07-24 23:34:46 -0400

    Added swift

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,6 +1,7 @@
 export GOPATH=$HOME/go
+export SWIFT_ROOT=/opt/swift
 export ANDROID_HOME=/usr/local/opt/android-sdk
 export NVM_DIR="$HOME/.nvm"
 
 # path, the 0 in the filename causes this to load first
-export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr:./node_modules:$HOME/bin:$GOPATH/bin
+export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr:./node_modules:$HOME/bin:$GOPATH/bin:$SWIFT_ROOT/usr/bin
